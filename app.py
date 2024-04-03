3d8dcbca122a4c5cc2aec739c242a8f5581e7da3
Rishabh J <rishabhj1470@gmail.com>
2024-04-03 23:34:38 +0530

    app.py updates

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ app = Flask(__name__)
 #creating route
 @app.route("/" , methods=["GET","POST"])# the / route accepts both get & post
 def home():
+    '''
     text = ""
     if request.method == "POST":
-        text = request.form.get('email-content')
-    return render_template("index.html", text=text)
+        text = request.form.get('email-content') 
+    '''
+    return render_template("index.html" ''', text=text''')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email_text = request.form.get("email-content")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
