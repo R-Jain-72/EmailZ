07d7dd98e719b450193f8f6ae7069497353e47c5
Rishabh J <rishabhj1470@gmail.com>
2024-04-04 22:53:57 +0530

    tokenizer setup

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import pickle
+
+tokenizer = pickle.load(open("cv.pkl", "rb"))#rb -> read binary
+model = pickle.load(open("cfl.pkl", "rb"))#clf -> classifier(model)
 
 app = Flask(__name__)
 
@@ -15,7 +19,11 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     email_text = request.form.get("email-content")
-    return render_template("index.html")
+    #tokenize the text
+    token_email = tokenizer.transform(email_text)
+    prediction = model.predict(token_email)
+    prediction = 1 if prediction == 1 else -1
+    return render_template("index.html", prediction=prediction, text=email_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
