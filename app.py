943e478e14abb2d14b8f0ec0b2c8818d98af54cf
Rishabh J <rishabhj1470@gmail.com>
2024-04-05 23:43:07 +0530

    project in last stages

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from utils import model_predict
 import pickle
 
-tokenizer = pickle.load(open("cv.pkl", "rb"))#rb -> read binary
-model = pickle.load(open("cfl.pkl", "rb"))#clf -> classifier(model)
+tokenizer = pickle.load(open("models/cv.pkl", "rb"))#rb -> read binary
+model = pickle.load(open("models/clf.pkl", "rb"))#clf -> classifier(model)
 
 app = Flask(__name__)
 
@@ -14,16 +15,24 @@ def home():
     if request.method == "POST":
         text = request.form.get('email-content') 
     '''
-    return render_template("index.html" ''', text=text''')
+    #return render_template("index.html" ''', text=text''')
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    email_text = request.form.get("email-content")
+    email = request.form.get('content')
     #tokenize the text
-    token_email = tokenizer.transform(email_text)
-    prediction = model.predict(token_email)
-    prediction = 1 if prediction == 1 else -1
-    return render_template("index.html", prediction=prediction, text=email_text)
+    token_email = tokenizer.transform([email])
+    prediction = model_predict(email)
+    return render_template("index.html", prediction=prediction, email=email)
+
+# Create an API endpoint
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    prediction = model_predict(email)
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
 
 if __name__ == "__main__":
     app.run(debug=True)
